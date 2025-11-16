de5c9c3538865ab23b8cc3fff9b0581e81ff39c6
Danishk <danishksinha@gmail.com>
2025-11-16 21:08:59 +0530

    Done

---
+++ To_Arrow_Function/index.js
@@ -0,0 +1,16 @@
+// ``Function Given:```
+
+
+// solution
+
+let multiply = (a=1, b=1) => {
+    return a*b;
+}
+
+console.log(multiply(5));
+console.log(multiply(5, 4));
+
+// function multiply(a , b ) {
+
+//   return a * b;
+// }
